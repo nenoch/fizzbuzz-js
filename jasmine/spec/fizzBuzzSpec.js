6ea225b954e0d34fb1d7b7d184c61d9be0f024fc
Irene Canuti <irene.canuti@gmail.com>
2017-01-30 18:25:59 +0000

    Completed FizzBuzz and tested with Jasmine

--- jasmine/spec/fizzBuzzSpec.js
+++ jasmine/spec/fizzBuzzSpec.js
@@ -10,6 +10,34 @@ describe('Fizzbuzz', function() {
       expect(fizzbuzz.isDivisibleByThree(3)).toBe(true);
     });
 
+    it('divisible by 5', function() {
+      expect(fizzbuzz.isDivisibleByFive(5)).toBe(true);
+    });
+
+    it('divisible by 3 and 5', function() {
+      expect(fizzbuzz.isDivisibleByFifteen(15)).toBe(true);
+    });
+
+  });
+
+  describe('screms different strings', function() {
+
+    it('screms Fizz if divisible by 3', function() {
+      expect(fizzbuzz.screams(3)).toEqual('Fizz');
+    });
+
+    it('screms Buzz if divisible by 5', function() {
+      expect(fizzbuzz.screams(5)).toEqual('Buzz');
+    });
+
+    it('screms FizzBuzz if divisible by 3 and 5', function() {
+      expect(fizzbuzz.screams(15)).toEqual('FizzBuzz');
+    });
+
+    it('returns the number if not divisible either by 3 or 5', function() {
+      expect(fizzbuzz.screams(2)).toEqual(2);
+    });
+
   });
 
 });
--- jasmine/src/fizzBuzz.js
+++ jasmine/src/fizzBuzz.js
@@ -2,10 +2,31 @@
 
 var Fizzbuzz = function() {};
 
-Fizzbuzz.prototype._isDivisibleBy = function(number, divisor) {
-  return (number % divisor === 0);
+Fizzbuzz.prototype._isDivisibleBy = function(num, divisor) {
+  return (num % divisor === 0);
 };
 
 Fizzbuzz.prototype.isDivisibleByThree = function(num) {
-  return (num % 3 === 0);
+  return this._isDivisibleBy(num, 3);
+};
+
+Fizzbuzz.prototype.isDivisibleByFive = function(num) {
+  return this._isDivisibleBy(num, 5);
+};
+
+Fizzbuzz.prototype.isDivisibleByFifteen = function(num) {
+  return this._isDivisibleBy(num, 15);
+};
+
+Fizzbuzz.prototype.screams = function(num) {
+  if (this.isDivisibleByFifteen(num)) {
+    return "FizzBuzz";
+  }
+  if (this.isDivisibleByThree(num)) {
+    return "Fizz";
+  }
+  if (this.isDivisibleByFive(num)) {
+    return "Buzz";
+  }
+  return num;
 };
